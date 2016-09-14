08470f909bb49ea4bf8aed01f613c939abca0232
quentin.luo <1131915347@qq.com>
2016-09-14 15:10:23 +0800

    test

--- src/SilexLearn/Controller/DefaultController.php
+++ src/SilexLearn/Controller/DefaultController.php
@@ -40,7 +40,6 @@ class DefaultController implements ControllerProviderInterface
         $code = $request->get('code');
         //https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx0243cccf60129301&secret=57df18c794809b8424f24344d76097fb&code=CODE&grant_type=authorization_code
         $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx0243cccf60129301&secret=57df18c794809b8424f24344d76097fb&code={$code}&grant_type=authorization_code";
-        $get = new \GuzzleHttp\Psr7\Request('get', $url);
-        return $get->getBody();
+        return print_r(file_get_contents($url));
     }
 }
