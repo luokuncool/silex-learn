ef2e9f2bef84f08fa8b19066f1ababbb5ab77624
quentin.luo <1131915347@qq.com>
2017-12-19 18:20:15 +0800

    redis test

--- tests/DemoTest.php
+++ tests/DemoTest.php
@@ -6,4 +6,13 @@ class DemoTest extends \PHPUnit\Framework\TestCase
     {
         $this->assertEquals(1, 1);
     }
+
+    /** @test */
+    public function redis()
+    {
+        $redis = new Redis();
+        $redis->connect('localhost');
+        $redis->set('foo', 'bar');
+        $this->assertEquals('bar', $redis->get('foo'));
+    }
 }
